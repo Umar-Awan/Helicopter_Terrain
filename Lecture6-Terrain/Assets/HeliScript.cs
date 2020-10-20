23cde1d80dbf1ea0462309f36f2093bd351d1d95
Umar <umaarawan19@gmail.com>
2020-10-20 08:37:08 +0500

    Helicopter on terrain
    Bullets are not vanishing from the scene

---
+++ Lecture6-Terrain/Assets/HeliScript.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class HeliScript : MonoBehaviour
+{
+    public GameObject bullet;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0, 0, 0.3f);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(0, 0, -0.3f);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(0, 0.5f, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(0, -0.5f, 0);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 0.1f, 0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, -0.1f, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(0, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(0, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.Rotate(-3, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.Rotate(3, 0, 0);
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            transform.Rotate(3, 0, 0);
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            transform.Rotate(-3, 0, 0);
+        }
+        /*if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.Rotate(0, 0, 3);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.Rotate(0, 0, -3);
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            transform.Rotate(0, 0, 3);
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            transform.Rotate(0, 0, -3);
+        }*/
+        if(Input.GetKeyDown(KeyCode.X))
+        {
+            Vector3 myposition = transform.position;
+            myposition.y -= 10;
+            Instantiate(bullet, myposition, transform.rotation);
+        }
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        print("Collision!!!!!!! " + collision.gameObject.name);
+        if (!collision.gameObject.name.StartsWith("Terrain"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
---
+++ Lecture6-Terrain/Assets/bulletScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0, 0.5f);
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (!collision.gameObject.name.StartsWith("Terrain"))
+        {
+            Destroy(collision.gameObject);
+        }
+        Destroy(transform.gameObject);
+    }
+
+    /*private void OnTriggerEnter(Collider col)
+    {
+        if (!col.gameObject.name.StartsWith("Terrain"))
+        {
+            Destroy(col.gameObject);
+        }
+
+    }*/
+}
